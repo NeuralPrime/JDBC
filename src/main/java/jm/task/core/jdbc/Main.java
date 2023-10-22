9d933bd8530e6c9ae93ebec60431c33d9989a858
mlcato123 <redviter69@gmail.com>
2023-10-22 18:23:45 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+        userService.saveUser("Petya", "Ivanov", (byte) 25);
+        userService.saveUser("Marya", "Sidorova", (byte) 69);
+        userService.saveUser("Ivan", "Petrov", (byte) 37);
+        userService.saveUser("Marina", "Kukushkina", (byte) 31);
+        userService.removeUserById(2);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "user";
+    private static Connection connection = null;
+
+
+    public static Connection getConnection() {
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                System.out.println("Connected to the database.");
+            } catch (SQLException e) {
+                System.out.println("Failed to connect to the database.");
+                e.printStackTrace();
+            }
+        return connection;
+    }
+
+
+
+
 }
